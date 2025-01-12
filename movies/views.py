53345863e61d1f8c438692fffb242daf214d708f
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-12 13:25:08 +0000

    implemented index and base html files | paginated dislpay for movie catalog

--- movies/views.py
+++ movies/views.py
@@ -6,10 +6,10 @@ class MovieListView(generic.ListView):
     model = Movie
 ##    template_name = "movie/movie_list.html"
 ##    context_object_name = "movies"
-    queryset = Movie.objects.all().order_by("Year")
-    template_name = "movie/movie_list.html"
-    context_object_name = "movies"
-    
+    queryset = Movie.objects.all().order_by("Year") # filter year from oldest to newest
+    template_name = "movie/index.html"
+#    context_object_name = "movies"
+    paginate_by = 12    
 
     
     
