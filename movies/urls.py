8922fe8fe83479dc288c7b990a4093109d58df4e
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-12 12:36:49 +0000

    fix displaying of models on homepage #2

--- movies/urls.py
+++ movies/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.MovieListView.as_view(), name="movie_list"),
+    path("", views.MovieListView.as_view(), name="home"),
 ]
--- movies/views.py
+++ movies/views.py
@@ -4,8 +4,13 @@ from .models import Movie
 
 class MovieListView(generic.ListView):
     model = Movie
+##    template_name = "movie/movie_list.html"
+##    context_object_name = "movies"
+    queryset = Movie.objects.all()
     template_name = "movie/movie_list.html"
     context_object_name = "movies"
+    
+
     
     
     
