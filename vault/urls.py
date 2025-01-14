91cba375144d27eb18c215ca998a7e7d1044d484
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-14 12:50:23 +0000

    added authentication

--- vault/urls.py
+++ vault/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path("accounts/", include("allauth.urls")),
     path("", include("movies.urls"), name="movie-urls"),
     path('admin/', admin.site.urls),
 ]
