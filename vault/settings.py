486823ab6e3add15974a58a4ea34ef9f2722113d
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-18 16:24:54 +0000

    fix: Django static files

--- vault/settings.py
+++ vault/settings.py
@@ -71,6 +71,8 @@ MIDDLEWARE = [
     'allauth.account.middleware.AccountMiddleware',
 ]
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
 ROOT_URLCONF = 'vault.urls'
 
 TEMPLATES = [
@@ -153,8 +155,8 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-# MEDIA_URL = '/media/'
-# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
