8458bc23565b6a1accd2bba2691c31b0237ac21e
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-20 18:51:42 +0000

    amended error in CRUD functionality

--- movies/views.py
+++ movies/views.py
@@ -19,7 +19,7 @@ class MovieListView(generic.ListView):
 def post_detail(request, slug):
     queryset = Movie.objects.filter(status=1)
     movie = get_object_or_404(Movie, slug=slug)
-    comments = movie.comments.filter(approved=True).order_by('-created_on')
+    comments = movie.comments.order_by('-created_on')
     comment_count = comments.count()
     rating_form = RatingForm()
     user_rating = None
@@ -66,28 +66,38 @@ def post_detail(request, slug):
     })
 
 def comment_edit(request, slug, comment_id):
+    queryset = Movie.objects.filter(status=1)
+    movie = get_object_or_404(queryset, slug=slug)
+    comment = get_object_or_404(Comment, pk=comment_id)
     if request.method == "POST":
-        queryset = Movie.objects.filter(status=1)
-        post = get_object_or_404(queryset, slug=slug)
-        comment = get_object_or_404(Comment, pk=comment_id)
         comment_form = CommentForm(data=request.POST, instance=comment)
         if comment_form.is_valid() and comment.author == request.user:
             comment = comment_form.save(commit=False)
-            comment.movie = post
+            comment.movie = movie
             comment.approved = False
             comment.save()
-            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+            messages.add_message(request, messages.SUCCESS, 'Comment updated and awaiting approval')
         else:
-            messages.add_message(request, messages.ERROR, 'Error updating comment!')
-    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
+            messages.add_message(request, messages.ERROR, 'Error updating comment')
+    else:
+        comment_form = CommentForm(instance=comment)
+    return render(request, "movie/post_detail.html", {
+        "movie": movie,
+        "comments": movie.comments.order_by('-created_on'),
+        "comment_count": movie.comments.count(),
+        "comment_form": comment_form,
+        "rating_form": RatingForm(),
+        "user_rating": Rating.objects.filter(movie=movie, user=request.user).first(),
+        "average_rating": movie.average_rating(),
+    })
 
 def comment_delete(request, slug, comment_id):
     queryset = Movie.objects.filter(status=1)
-    post = get_object_or_404(queryset, slug=slug)
+    movie = get_object_or_404(queryset, slug=slug)
     comment = get_object_or_404(Comment, pk=comment_id)
     if comment.author == request.user:
         comment.delete()
-        messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
+        messages.add_message(request, messages.SUCCESS, 'Comment deleted')
     else:
-        messages.add_message(request, messages.ERROR, 'You can only delete your own comments!')
+        messages.add_message(request, messages.ERROR, 'You can only delete your own comments')
     return HttpResponseRedirect(reverse('post_detail', args=[slug]))
