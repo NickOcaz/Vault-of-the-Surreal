fcb46644bc71da6f1a9f63b9c23b113282f4cd09
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-22 13:02:12 +0000

    fix: element in scope but a p end tag seen.

---
+++ movies/static/js/comments.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var deleteModal = document.getElementById('deleteModal');
+    deleteModal.addEventListener('show.bs.modal', function (event) {
+        var button = event.relatedTarget;
+        var commentId = button.getAttribute('data-comment-id');
+        var confirmDeleteButton = deleteModal.querySelector('#confirmDeleteButton');
+        confirmDeleteButton.href = confirmDeleteButton.href.replace('0', commentId);
+    });
+});
