d38e984bebe82bb9dd78660e7839bad977440532
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-22 12:35:33 +0000

    css style: movie pages, comment section and forms

--- staticfiles/js/comments.js
+++ staticfiles/js/comments.js
@@ -1,28 +1,36 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const editButtons = document.getElementsByClassName("btn-edit");
-  const commentText = document.getElementById("id_body");
-  const commentForm = document.getElementById("commentForm");
-  const submitButton = document.getElementById("submitButton");
-  const deleteModalElement = document.getElementById("deleteModal");
-  const deleteModal = new bootstrap.Modal(deleteModalElement);
-  const deleteButtons = document.getElementsByClassName("btn-delete");
-  const deleteConfirm = document.getElementById("confirmDeleteButton");
+document.addEventListener('DOMContentLoaded', function () {
+    const editButtons = document.getElementsByClassName("btn-edit");
+    const commentText = document.getElementById("id_body");
+    const commentForm = document.getElementById("commentForm");
+    const submitButton = document.getElementById("submitButton");
+    const deleteModalElement = document.getElementById("deleteModal");
+    const deleteModal = new bootstrap.Modal(deleteModalElement);
+    const deleteButtons = document.getElementsByClassName("btn-delete");
+    const deleteConfirm = document.getElementById("confirmDeleteButton");
 
-  for (let button of editButtons) {
-      button.addEventListener("click", (e) => {
-          let commentId = e.target.getAttribute("comment_id");
-          let commentContent = document.getElementById(`comment${commentId}`).innerText;
-          commentText.value = commentContent;
-          submitButton.innerText = "Update";
-          commentForm.setAttribute("action", `edit_comment/${commentId}`);
-      });
-  }
+    for (let button of editButtons) {
+        button.addEventListener("click", (e) => {
+            let commentId = e.target.getAttribute("comment_id");
+            let commentContent = document.getElementById(`comment${commentId}`).innerText;
+            commentText.value = commentContent;
+            submitButton.innerText = "Update";
+            commentForm.setAttribute("action", `edit_comment/${commentId}`);
+        });
+    }
 
-  for (let button of deleteButtons) {
-      button.addEventListener("click", (e) => {
-          let commentId = e.target.getAttribute("data-comment-id");
-          deleteConfirm.href = `delete_comment/${commentId}`;
-          deleteModal.show();
-      });
-  }
+    for (let button of deleteButtons) {
+        button.addEventListener("click", (e) => {
+            let commentId = e.target.getAttribute("data-comment-id");
+            deleteConfirm.href = `delete_comment/${commentId}`;
+            deleteModal.show();
+        });
+    }
 });
+
+var deleteModal = document.getElementById('deleteModal');
+deleteModal.addEventListener('show.bs.modal', function (event) {
+    var button = event.relatedTarget;
+    var commentId = button.getAttribute('data-comment-id');
+    var confirmDeleteButton = deleteModal.querySelector('#confirmDeleteButton');
+    confirmDeleteButton.href = "{% url 'comment_delete' slug=movie.slug comment_id=0 %}".replace('0', commentId);
+    });
