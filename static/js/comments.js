d2129696ae48056e54eb5bfe9d913a5994aa8bfb
Nick O'Carroll <6e3u6ntg@students.codeinstitute.net>
2025-01-23 11:00:17 +0000

    fix: edit comment to only parse comment data

--- static/js/comments.js
+++ static/js/comments.js
@@ -10,11 +10,11 @@ document.addEventListener('DOMContentLoaded', function () {
 
     for (let button of editButtons) {
         button.addEventListener("click", (e) => {
-            let commentId = e.target.getAttribute("comment_id");
-            let commentContent = document.getElementById(`comment${commentId}`).innerText;
+            let commentId = e.target.getAttribute("data-comment_id");
+            let commentContent = document.getElementById(`comment${commentId}`).innerText
             commentText.value = commentContent;
             submitButton.innerText = "Update";
-            commentForm.setAttribute("action", `edit_comment/${commentId}`);
+            commentForm.setAttribute("action", `edit_comment/${commentId}/`);
         });
     }
 
